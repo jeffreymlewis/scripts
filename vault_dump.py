087e70d780d6e186aacdeb74c48a2cf607d0c43a
Jeffrey Lewis <ez060961@gmail.com>
2023-10-20 08:26:18 -0700

    Update vault_dump.py to conform to Google style guide.
    Using autopep8!

--- vault_dump.py
+++ vault_dump.py
@@ -4,22 +4,24 @@
 import json
 import os
 
+
 def get_all_kv_secrets(path):
-    """List all secrets in the given path from vault KV engine."""
-    output = os.popen(f"vault kv list -format=json {path}").read()
+  """List all secrets in the given path from vault KV engine."""
+  output = os.popen(f"vault kv list -format=json {path}").read()
 
-    # If this is a leaf node, run 'vault kv get'
-    if output.startswith("{"):
-        data = os.popen(f"vault kv get -format=json {path}").read()
-        print (data)
+  # If this is a leaf node, run 'vault kv get'
+  if output.startswith("{"):
+    data = os.popen(f"vault kv get -format=json {path}").read()
+    print(data)
 
-    # not a leaf node
-    else:
-        subdirs = json.loads(output)
-        for subdir in subdirs:
-            subdir_path = f"{path}/{subdir}"
-            print(subdir_path)
-            get_all_kv_secrets(subdir_path)
+  # not a leaf node
+  else:
+    subdirs = json.loads(output)
+    for subdir in subdirs:
+      subdir_path = f"{path}/{subdir}"
+      print(subdir_path)
+      get_all_kv_secrets(subdir_path)
+
 
 if __name__ == "__main__":
-    get_all_kv_secrets("secrets/")
+  get_all_kv_secrets("secrets/")
